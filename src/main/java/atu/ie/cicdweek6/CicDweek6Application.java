a14e379ffc7fae9e5c4b281d5cf6afea3f8f1f6a
ANZELINAHAREVICA-STU <g00414991@atu.ie>
2025-11-05 14:57:25 +0000

    Created Week6 Application

---
+++ src/main/java/atu/ie/cicdweek6/CicDweek6Application.java
@@ -0,0 +1,13 @@
+package atu.ie.cicdweek6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDweek6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDweek6Application.class, args);
+    }
+
+}
